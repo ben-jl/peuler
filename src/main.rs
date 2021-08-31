71bbcb225c19e1464e035769655db39bbf60ffa1
Ben LeValley <ben.levalley@gmail.com>
2021-08-31 16:20:11 -0500

    Problem 2: Even Fibonacci Numbers

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 fn main() {
-    let result = calc_multiples_of_3_and_5_below_n(1000);
+    let result = even_fibs_below_n(4000000);
     println!("{}", result);
 }
 
-fn calc_multiples_of_3_and_5_below_n(n : i32) -> i32 {
+fn even_fibs_below_n(n : i32) -> i32 {
     let mut sum : i32 = 0;
-    for number in (1..(n-1)).rev() {
-        if number % 3 == 0 || number % 5 == 0 {
-            sum = sum + number;
+    let mut last : i32 = 1;
+    let mut curr : i32 = 1;
+
+    while curr < n {
+        if curr % 2 == 0 {
+            sum = sum + curr;
         }
+        curr = last + curr;
+        last = curr - last;
     }
-    return sum;
+
+    sum
 }
