0cfd4f14268663f83020f30bbe753e23c864eaa7
Ben LeValley <ben.levalley@gmail.com>
2021-09-03 16:43:18 -0500

    Problem 3: Largest Prime Factor

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,18 @@ use std::convert::TryInto;
 pub fn primes_less_than_n(n:u64) -> Vec<u64> {
     let vsize :usize = (n-1).try_into().unwrap();
 
-    let mut vec = Vec::with_capacity(vsize);
-    vec.resize(vsize, 1);
+    let mut vec : Vec<bool> = Vec::with_capacity(vsize);
+    vec.resize(vsize, true);
 
     let sqrtn : u64 = (n as f64).sqrt() as u64 + 1;
 
     for i in (2..sqrtn).rev() {
         let idx = i - 2;
-        if vec[idx as usize] == 1 {
+        if vec[idx as usize] {
             let mut j = i.pow(2);
             while j <= n {
                 let jdx = j - 2;
-                vec[jdx as usize] = 0;
+                vec[jdx as usize] = false;
                 j = j + i;
             }
         }
@@ -22,7 +22,7 @@ pub fn primes_less_than_n(n:u64) -> Vec<u64> {
 
     let mut res : Vec<u64> = Vec::new();
     for (i, x) in vec.iter().enumerate() {
-        if *x == 1  {
+        if *x  {
             res.push(i as u64 + 2)
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod lib;
 
 fn main() {
-    let result = lib::primes_less_than_n(20);
-    println!("{:?}", result);
+    let primes = lib::primes_less_than_n(775146);
+    for p in primes.iter().rev() {
+        if 600851475143 % p == 0 {
+            println!("{}",p);
+            break;
+        }
+    }
 }
